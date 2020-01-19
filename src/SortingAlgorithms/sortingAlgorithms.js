c4bdd340234bd8dcfbfc2797d3300a14fb953a3f
Nada Zeini <nada.elzeini@sjsu.edu>
2020-01-19 01:01:05 -0800

    bubble sort // numbers swap

--- src/SortingAlgorithms/sortingAlgorithms.js
+++ src/SortingAlgorithms/sortingAlgorithms.js
@@ -1,125 +1,92 @@
 /* Merge Sort
  ******************************************************************************************************************************************************************************************************************************************************************************************************************** */
 export function getMergeSortAnimations(array) {
-  const animations = [];
-  if (array.length <= 1) 
-    return array;
-  const auxiliaryArray = array.slice();
-  mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
-  return animations;
+	const animations = [];
+	if (array.length <= 1) return array;
+	const auxiliaryArray = array.slice();
+	mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+	return animations;
 }
 
-function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, animations) {
-  if (startIdx === endIdx) 
-    return;
-  const middleIdx = Math.floor((startIdx + endIdx) / 2);
-  mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
-  mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
-  doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+function mergeSortHelper(
+	mainArray,
+	startIdx,
+	endIdx,
+	auxiliaryArray,
+	animations
+) {
+	if (startIdx === endIdx) return;
+	const middleIdx = Math.floor((startIdx + endIdx) / 2);
+	mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+	mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+	doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
 }
 
-function doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations) {
-  let k = startIdx;
-  let i = startIdx;
-  let j = middleIdx + 1;
-  while (i <= middleIdx && j <= endIdx) {
-    // These are the values that we're comparing; we push them once
-    // to change their color.
-    animations.push([i, j]);
-    // These are the values that we're comparing; we push them a second
-    // time to revert their color.
-    animations.push([i, j]);
-    if (auxiliaryArray[i] <= auxiliaryArray[j]) {
-      // We overwrite the value at index k in the original array with the
-      // value at index i in the auxiliary array.
-      animations.push([
-        k, auxiliaryArray[i]
-      ]);
-      mainArray[k++] = auxiliaryArray[i++];
-    } else {
-      // We overwrite the value at index k in the original array with the
-      // value at index j in the auxiliary array.
-      animations.push([
-        k, auxiliaryArray[j]
-      ]);
-      mainArray[k++] = auxiliaryArray[j++];
-    }
-  }
-  while (i <= middleIdx) {
-    // These are the values that we're comparing; we push them once
-    // to change their color.
-    animations.push([i, i]);
-    // These are the values that we're comparing; we push them a second
-    // time to revert their color.
-    animations.push([i, i]);
-    // We overwrite the value at index k in the original array with the
-    // value at index i in the auxiliary array.
-    animations.push([
-      k, auxiliaryArray[i]
-    ]);
-    mainArray[k++] = auxiliaryArray[i++];
-  }
-  while (j <= endIdx) {
-    // These are the values that we're comparing; we push them once
-    // to change their color.
-    animations.push([j, j]);
-    // These are the values that we're comparing; we push them a second
-    // time to revert their color.
-    animations.push([j, j]);
-    // We overwrite the value at index k in the original array with the
-    // value at index j in the auxiliary array.
-    animations.push([
-      k, auxiliaryArray[j]
-    ]);
-    mainArray[k++] = auxiliaryArray[j++];
-  }
+function doMerge(
+	mainArray,
+	startIdx,
+	middleIdx,
+	endIdx,
+	auxiliaryArray,
+	animations
+) {
+	let k = startIdx;
+	let i = startIdx;
+	let j = middleIdx + 1;
+	while (i <= middleIdx && j <= endIdx) {
+		animations.push([i, j]);
+		animations.push([i, j]);
+		if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+			animations.push([k, auxiliaryArray[i]]);
+			mainArray[k++] = auxiliaryArray[i++];
+		} else {
+			animations.push([k, auxiliaryArray[j]]);
+			mainArray[k++] = auxiliaryArray[j++];
+		}
+	}
+	while (i <= middleIdx) {
+		animations.push([i, i]);
+		animations.push([i, i]);
+		animations.push([k, auxiliaryArray[i]]);
+		mainArray[k++] = auxiliaryArray[i++];
+	}
+	while (j <= endIdx) {
+		animations.push([j, j]);
+		animations.push([j, j]);
+		animations.push([k, auxiliaryArray[j]]);
+		mainArray[k++] = auxiliaryArray[j++];
+	}
 }
 /* Bubble Sort
  ******************************************************************************************************************************************************************************************************************************************************************************************************************** */
-
-export function bubbleSort(mainArray, speed) {
-  var array = mainArray;
-  console.log(array);
-  var isSorted = false;
-  var iteration = 0;
-  while (!isSorted) {
-    let lastUnsorted = array.length - 1;
-    isSorted = true;
-    for (let i = 0; i < lastUnsorted - iteration; i++) {
-      if (array[i] > array[i + 1]) {
-        swap(array, array[i], array[i + 1]);
-        isSorted = false;
-      }
-    }
-    iteration++;
-  }
-  console.log(array);
-  //handleBubbleSortAnimations(animations, animate, array, speed);
-  return array;
+export function getBubbleSortAnimations(array) {
+	let animations = [];
+	let auxArray = array.slice();
+	doBubbleSort(auxArray, animations);
+	array = auxArray;
+	return animations;
 }
+function doBubbleSort(auxArray, animations) {
+	for (let i = 0; i < auxArray.length - 1; i++) {
+		for (let j = 0; j < auxArray.length - 1 - i; j++) {
+			//push once to color
+			animations.push([j, j + 1]);
+			//push second time to uncolor?
+			animations.push([j, j + 1]);
+			if (auxArray[j] > auxArray[j + 1]) {
+				//swap
+				animations.push([j + 1, auxArray[j]]);
+				animations.push([j, auxArray[j + 1]]);
 
-export function swap(array, i, j) {
-  const tmp = array[i];
-  array[i] = array[j];
-  array[j] = tmp;
+				let tmp = auxArray[j + 1];
+				auxArray[j + 1] = auxArray[j];
+				auxArray[j] = tmp;
+			} else {
+				// if -1 then there was no swap
+				animations.push([-1, -1]);
+				animations.push([-1, -1]);
+				//not sure if twice
+			}
+		}
+	}
 }
-
-/*function handleBubbleSortAnimations(animations, animate, array, speed) {
-  if (!animations.length) {
-    animate(setCurrentBubbleTwo(array.map((num, index) => index)));
-    setTimeout(() =>{
-      animate(setCurrentBubbleTwo([]));
-      animate(setCurrentSorted(array.map((num,index) => index)));
-      animate(setRunning(false));
-    },900);
-    return;
-  }
-  let animateFunction = animations[0].length > 3 ?
-    setArray : animations[0].length === 3 || animations[0].length === 0 ?
-      setCurrentSwappers : animations[0].length === 2 && typeof animations[0][0] === "boolean" ?
-        setCurrentSorted : setCurrentBubbleTwo;
-  animate(animateFunction(animations.shift()));
-  setTimeout(() => {
-    handleBubbleSortAnimations(animations, animate, array, speed);
-  }, speed);
-}*/
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,153 +1,212 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./SortingVisualizer.css";
-import {getMergeSortAnimations} from "../SortingAlgorithms/sortingAlgorithms.js";
-import {swap} from "../SortingAlgorithms/sortingAlgorithms.js";
-//import {bubbleSort} from "../SortingAlgorithms/sortingAlgorithms.js";
-const ANIMATION_SPEED_MS = 2;
+import React from 'react';
+import './SortingVisualizer.css';
+import {
+	getMergeSortAnimations,
+	getBubbleSortAnimations
+} from '../SortingAlgorithms/sortingAlgorithms.js';
 
-// This is the main color of the array bars.
-let PRIMARY_COLOR = "orange";
+const ANIMATION_SPEED_MS = 5;
+const PRIMARY_COLOR = 'rgb(195, 146, 223)';
+const SECONDARY_COLOR = 'purple';
+const ARRAY_LENGTH = 35;
 
-// This is the color of array bars that are being compared throughout the animations.
-const SECONDARY_COLOR = "purple";
 export default class SortingVisualizer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      array: []
-    };
-  }
-  componentDidMount() {
-    //first time loading
-    this.resetArray();
-  }
-  resetArray() {
-    //creates array
-    const array = [];
-    for (let i = 0; i < 20; i++) {
-      array.push(randomInt(5, 500));
-    }
-    this.setState({array});
-    PRIMARY_COLOR = "rgb(195, 146, 223)";
-  }
-  getValue(val) {
-    return this.state.array[val];
-  }
-  //******************************************************************************************************************************************************************************************************************************************************************************************************************** */
-  mergeSort() {
-    const animations = getMergeSortAnimations(this.state.array);
-    for (let i = 0; i < animations.length; i++) {
-      const arrayBars = document.getElementsByClassName("array-bar");
-      const arrayBarsWithNo = document.getElementsByClassName("array-container");
-      let nums = document.getElementById("numbers");
-      const isColorChange = i % 3 !== 2;
-      if (isColorChange) {
-        const [barOneIdx, barTwoIdx] = animations[i];
-        const barOneStyle = arrayBars[barOneIdx].style;
-        const barTwoStyle = arrayBars[barTwoIdx].style;
-        const barOneStyleNo = arrayBarsWithNo[barOneIdx].style;
-        const barTwoStyleNo = arrayBarsWithNo[barTwoIdx].style;
-        PRIMARY_COLOR = "orange";
-        const color = i % 3 === 0
-          ? SECONDARY_COLOR
-          : PRIMARY_COLOR;
-        setTimeout(() => {
-          barOneStyle.backgroundColor = color;
-          barTwoStyle.backgroundColor = color;
-          barOneStyleNo.backgroundColor = color;
-          barTwoStyleNo.backgroundColor = color;
-          //num1.textContent = num2Text;
-          //num2.textContent = num1.textContent;
-        }, i * ANIMATION_SPEED_MS);
-      } else {
-        setTimeout(() => {
-          const [barOneIdx, newHeight] = animations[i];
-          const barOneStyle = arrayBars[barOneIdx].style;
-          barOneStyle.height = `${newHeight}px`;
-          //nums.innerHTML = newHeight;
-        }, i * ANIMATION_SPEED_MS);
-      }
-    }
-  }
-  //******************************************************************************************************************************************************************************************************************************************************************************************************************** */
+	constructor(props) {
+		super(props);
+		this.state = {
+			array: [],
+			sorted: false
+		};
+	}
+	componentDidMount() {
+		//first time loading
+		this.resetArray();
+	}
+	resetArray() {
+		//creates array
+		const array = [];
+		for (let i = 0; i < ARRAY_LENGTH; i++) {
+			array.push(randomInt(5, 400));
+		}
+		this.setState({ array });
+	}
+	getValue(val) {
+		return this.state.array[val];
+	}
 
-  //******************************************************************************************************************************************************************************************************************************************************************************************************************** */
-  bubbleSort() {
-    let array = this.state.array;
-    let swapped;
-    do {
-      swapped = false;
-      for (let i = 0; i < array.length; i++) {
-        const arrayBars = document.getElementsByClassName("array-bar");
-        const arrayBarsWithNo = document.getElementsByClassName("array-container");
-        if (array[i] > array[i + 1]) {
-          let tmp = array[i];
-          array[i] = array[i + 1];
-          array[i + 1] = tmp;
-          swapped = true;
-        }
-      }
-    } while (swapped);
-    return array;
-  }
-  getNext = i => {
-    console.log(i + 1);
-  };
+	//MERGE SORT
+	//******************************************************************************************************************************************************************************************************************************************************************************************************************** */
+	mergeSort() {
+		const animations = getMergeSortAnimations(this.state.array);
+		for (let i = 0; i < animations.length; i++) {
+			const arrayBars = document.getElementsByClassName('array-bar');
+			const arrayBarsWithNo = document.getElementsByClassName(
+				'array-container'
+			);
+			const numbersBars = document.getElementsByClassName('numbers');
+			const isColorChange = i % 3 !== 2;
+			if (isColorChange) {
+				const [barOneIdx, barTwoIdx] = animations[i];
+				const barOneStyle = arrayBars[barOneIdx].style;
+				const barTwoStyle = arrayBars[barTwoIdx].style;
+				const barOneStyleNo = arrayBarsWithNo[barOneIdx].style;
+				const barTwoStyleNo = arrayBarsWithNo[barTwoIdx].style;
+				const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+				setTimeout(() => {
+					barOneStyle.backgroundColor = color;
+					barTwoStyle.backgroundColor = color;
+					barOneStyleNo.backgroundColor = color;
+					barTwoStyleNo.backgroundColor = color;
+					//num1.textContent = num2Text;
+					//num2.textContent = num1.textContent;
+				}, i * ANIMATION_SPEED_MS);
+			} else {
+				//console.log(numbersBars[barOneIdx]);
+				setTimeout(() => {
+					const [barOneIdx, newHeight] = animations[i];
+					const barOneStyle = arrayBars[barOneIdx].style;
+					let newH = newHeight;
+					newH = newH.toString().replace('px', '');
+					numbersBars[barOneIdx].textContent = newH;
+					barOneStyle.height = `${newHeight}px`;
+				}, i * ANIMATION_SPEED_MS);
+			}
+		}
+	}
 
-  quickSort() {}
-  pancakeSort() {}
-  heapSort() {}
-  inserstionSort() {}
-  selectionSort() {}
+	//******************************************************************************************************************************************************************************************************************************************************************************************************************** */
 
-  render() {
-    const {array} = this.state;
-    const index = 0;
-    return (<div className="container">
-      <div className="nav-bar">
-        <div className="btn-group mr-2" role="group" aria-label="First group">
-          <button type="button" className="btn btn-secondary boot-background boot-color" onClick={() => this.resetArray()}>
-            Shuffle
-          </button>
-        </div>
-        <button type="button" className="btn btn-outline-secondary boot-background boot-color" onClick={() => this.mergeSort()}>
-          Merge Sort
-        </button>
-        <button type="button" className="btn btn-outline-secondary boot-background boot-color" onClick={() => this.quickSort()}>
-          Quick Sort
-        </button>
-        <button type="button" className="btn btn-outline-secondary boot-background boot-color" onClick={() => this.pancakeSort()}>
-          Pancake Sort
-        </button>
-        <button type="button" className="btn btn-outline-secondary boot-background boot-color" onClick={() => this.heapSort()}>
-          Heap Sort
-        </button>
-        <button type="button" className="btn btn-outline-secondary boot-background boot-color" onClick={() => this.bubbleSort()}>
-          Bubble Sort
-        </button>
-        <button type="button" className="btn btn-outline-secondary boot-background boot-color" onClick={() => this.selectionSort()}>
-          Selection Sort
-        </button>
-        <button type="button" className="btn btn-outline-secondary boot-background boot-color" onClick={() => this.inserstionSort()}>
-          Insertion Sort
-        </button>
-      </div>
+	//******************************************************************************************************************************************************************************************************************************************************************************************************************** */
+	bubbleSort() {
+		let array = this.state.array;
+		let animations = getBubbleSortAnimations(array);
+		for (let i = 0; i < animations.length; i++) {
+			const arrayBars = document.getElementsByClassName('array-bar');
+			const arrayBarsWithNo = document.getElementsByClassName(
+				'array-container'
+			);
+			const numbersBars = document.getElementsByClassName('numbers');
+			//color change is on ever 4 indexes: 0, 4, 8 ...
+			const colorChange = i % 4 === 0 || i % 4 === 1;
+			if (colorChange) {
+				const [barOneIdx, barTwoIdx] = animations[i];
+				const barOneStyle = arrayBars[barOneIdx].style;
+				const barTwoStyle = arrayBars[barTwoIdx].style;
+				const barOneStyleNo = arrayBarsWithNo[barOneIdx].style;
+				const barTwoStyleNo = arrayBarsWithNo[barTwoIdx].style;
+				const color = i % 4 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+				setTimeout(() => {
+					barOneStyle.backgroundColor = color;
+					barTwoStyle.backgroundColor = color;
+					barOneStyleNo.backgroundColor = color;
+					barTwoStyleNo.backgroundColor = color;
+				}, i * ANIMATION_SPEED_MS);
+			} else {
+				const [barOneIdx, newHeight] = animations[i];
+				if (barOneIdx === -1) {
+					continue;
+				}
+				const barOneStyle = arrayBars[barOneIdx].style;
+				let newH = newHeight;
+				newH = newH.toString().replace('px', '');
 
-      {
-        array.map((value, idx) => (<div className="array-container">
-          <div className="numbers" id="numbers">
-            {value}
-          </div>
-          <div className="array-bar" key={idx} style={{
-              height: `${value}px`
-            }}/>
-        </div>))
-      }
-    </div>);
-  }
+				//console.log(numbersBars[barOneIdx]);
+				setTimeout(() => {
+					numbersBars[barOneIdx].textContent = newH;
+					barOneStyle.height = `${newHeight}px`;
+				}, i * ANIMATION_SPEED_MS);
+			}
+		}
+	}
+
+	quickSort() {}
+	pancakeSort() {}
+	heapSort() {}
+	inserstionSort() {}
+	selectionSort() {}
+	render() {
+		const { array } = this.state;
+		return (
+			<div className='container'>
+				<div className='nav-bar'>
+					<div className='btn-group mr-2' role='group' aria-label='First group'>
+						<button
+							type='button'
+							className='btn btn-secondary boot-background boot-color'
+							onClick={() => this.resetArray()}
+						>
+							Shuffle
+						</button>
+					</div>
+					<button
+						type='button'
+						className='btn btn-outline-secondary boot-background boot-color'
+						onClick={() => this.mergeSort()}
+					>
+						Merge Sort
+					</button>
+					<button
+						type='button'
+						className='btn btn-outline-secondary boot-background boot-color'
+						onClick={() => this.quickSort()}
+					>
+						Quick Sort
+					</button>
+					<button
+						type='button'
+						className='btn btn-outline-secondary boot-background boot-color'
+						onClick={() => this.pancakeSort()}
+					>
+						Pancake Sort
+					</button>
+					<button
+						type='button'
+						className='btn btn-outline-secondary boot-background boot-color'
+						onClick={() => this.heapSort()}
+					>
+						Heap Sort
+					</button>
+					<button
+						type='button'
+						className='btn btn-outline-secondary boot-background boot-color'
+						onClick={() => this.bubbleSort()}
+					>
+						Bubble Sort
+					</button>
+					<button
+						type='button'
+						className='btn btn-outline-secondary boot-background boot-color'
+						onClick={() => this.selectionSort()}
+					>
+						Selection Sort
+					</button>
+					<button
+						type='button'
+						className='btn btn-outline-secondary boot-background boot-color'
+						onClick={() => this.inserstionSort()}
+					>
+						Insertion Sort
+					</button>
+				</div>
+
+				{array.map((value, idx) => (
+					<div className='array-container' key={idx}>
+						<div className='numbers' id='numbers' textcontent={value}>
+							{value}
+						</div>
+						<div
+							className='array-bar'
+							
+							style={{
+								height: `${value}px`
+							}}
+						/>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
-
 function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
