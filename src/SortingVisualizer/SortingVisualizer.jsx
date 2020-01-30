0cb69aa738cf95d67a437a8e853cbad9ee592e01
Nada Zeini <nada.elzeini@sjsu.edu>
2020-01-30 01:44:51 -0800

    adjusted fonts/array size

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -12,13 +12,13 @@ export default class SortingVisualizer extends React.Component {
     //this.selectSpeed = this.selectSpeed.bind(this);
     this.state = {
       array: [],
-      array_length: 37, //props.array_length,
-      speed: 3
+      array_length: 34, //props.array_length,
+      speed: 1
     };
     // this.toggle_length = this.toggle_length.bind(this);
   }
   toggle_length(val) {
-    console.log(val);
+    //console.log(val);
     this.setState({array_length: val});
     this.resetArray();
   }
@@ -32,7 +32,7 @@ export default class SortingVisualizer extends React.Component {
     // let len = this.state.array_length;
 
     for (let i = 0; i < this.state.array_length; i++) {
-      array.push(randomInt(5, 500));
+      array.push(randomInt(10, 500));
     }
     this.setState({array});
   }
@@ -82,6 +82,73 @@ export default class SortingVisualizer extends React.Component {
   }
   render() {
     const {array} = this.state;
+    //if array len is between is less or equal to 37
+    let bar_size = "";
+    const len = this.state.array_length;
+    let font_size = "";
+
+    //make a function later
+    //can be automated
+    if (len <= 33) {
+      bar_size = "30px";
+    }
+    if (len > 33 && len < 37) {
+      bar_size = "28px";
+    }
+    if (len >= 37 && len <= 40) {
+      bar_size = "25px";
+    }
+    if (len > 40 && len <= 43) {
+      bar_size = "22px";
+    }
+    if (len > 43 && len <= 47) {
+      bar_size = "20px";
+    }
+    if (len > 47 && len <= 50) {
+      bar_size = "18px";
+    }
+    if (len > 50 && len <= 53) {
+      bar_size = "17px";
+    }
+    if (len > 53 && len <= 58) {
+      bar_size = "16px";
+    }
+    if (len > 58 && len <= 64) {
+      bar_size = "14px";
+    }
+    if (len > 64 && len <= 66) {
+      bar_size = "13px";
+    }
+    if (len > 66 && len <= 72) {
+      bar_size = "12px";
+    }
+    if (len > 72 && len <= 80) {
+      bar_size = "11px";
+    }
+    if (len > 80 && len <= 90) {
+      bar_size = "10px";
+    }
+    if (len > 90 && len <= 100) {
+      bar_size = "9px";
+    }
+    if (len > 100 && len <= 120) {
+      bar_size = "7px";
+    }
+    if (len > 120 && len <= 137) {
+      bar_size = "6px";
+    }
+    if (len > 137 && len <= 150) {
+      bar_size = "5px";
+    }
+    if (len > 150 && len <= 183) {
+      bar_size = "4px";
+    }
+    if (len > 183 && len <= 220) {
+      bar_size = "3px";
+    }
+    if (len > 220 && len <= 280) {
+      bar_size = "2px";
+    }
     return (<div className="container">
       <div className="nav-bar">
         <div className="row justify-content-center pb-3 ">
@@ -140,12 +207,21 @@ export default class SortingVisualizer extends React.Component {
 
       {
         array.map((value, idx) => (<div className="array-container" key={idx}>
-          <div className="numbers" id="numbers" textcontent={value}>
+          <div className="array-bar" style={{
+              height: `${value}px`,
+              width: bar_size
+            }}/>
+          <div className="numbers" id="numbers" textcontent={value} style={this.state.array_length <= 37
+              ? {
+                fontSize: "px 1",
+                color: "white"
+              }
+              : {
+                display: "none"
+              }
+}>
             {value}
           </div>
-          <div className="array-bar" style={{
-              height: `${value}px`
-            }}/>
         </div>))
       }
     </div>);
